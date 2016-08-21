934a1b2deb753b3ed415cb34894375a4db97ce6d
geiser <geiser.gcc@gmail.com>
2016-08-21 18:25:26 -0300

    Change project name

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject br.usp.icmc.cljhozo/core "0.1.0-SNAPSHOT"
+(defproject br.usp.icmc.caed/cljhozo "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
